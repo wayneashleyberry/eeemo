4ffa76545c1194578fecbc0b851d6f984d4be0f5
wayneashleyberry <wayneashleyberry@gmail.com>
2020-05-28 14:11:59 +0100

    flags

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	ctx := context.Background()
+
+	var size string
+	var up, middle, down bool
+	middle, down = true, true
+
+	cmd := &cobra.Command{
+		Use: "eeemo",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if size != "mini" && size != "normal" && size != "maxi" {
+				return errors.New("invalid size")
+			}
+
+			fmt.Println(up, middle, down)
+
+			fmt.Println(size)
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVarP(&size, "size", "", "mini", "Valid sizes are 'mini', 'normal' and 'maxi'")
+	cmd.Flags().BoolVarP(&up, "up", "", true, "")
+	cmd.Flags().BoolVarP(&middle, "middle", "", true, "")
+	cmd.Flags().BoolVarP(&down, "down", "", false, "")
+
+	err := cmd.ExecuteContext(ctx)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
